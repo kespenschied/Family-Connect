255caa04546aa310d34648d32440d20f278a395c
SeanMat <smathew808@gmail.com>
2019-04-07 18:56:16 -0500

    Going to commit, push for first time, Tabs done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import './FirstPage.dart' as first;
 
-void main() => runApp(MyApp());
+void main() => runApp(new MaterialApp(home: new MyTabs()));
 
-class MyApp extends StatelessWidget {
+class MyTabs extends StatefulWidget {
+  @override
+  MyTabsState createState() => new MyTabsState();
+  
+}
+
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
+ 
+ TabController controller;
+
+ @override
+ void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+ 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('TrapsAreGay')),),));
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Favourites"),
+        backgroundColor: Colors.green, bottom: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+          new Tab(icon: Icon(Icons.home)),
+          new Tab(icon: Icon(Icons.looks_one)),
+          new Tab(icon: Icon(Icons.looks_two)),
+          new Tab(icon: Icon(Icons.looks_3))
+        ],),
+      ),
+    );
   }
-
 }
