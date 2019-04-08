b479080b6f1b2254933315d9b2e65f89294b555d
Chris <chris@chriss-mbp.home>
2019-04-07 23:53:21 -0500

    Basic layout of main landing page. Familiarizing

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Family Connect',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Home'),
     );
   }
 }
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      //_counter++;
     });
   }
 
@@ -70,37 +70,76 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      
+      body: 
+        Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: EdgeInsets.only(top: 20),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              'Welcome to Family Connect, Connie!', textScaleFactor: 2, textAlign: TextAlign.center,
             ),
-          ],
-        ),
-      ),
+          // Column(
+          //   children: <Widget>[
+            Row(
+              children: <Widget>[
+                // Padding(   
+                //   padding: EdgeInsets.fromLTRB(50, 20, 15, 20),
+                //   child:
+                //   Text('Chores', textScaleFactor: 1.5, textAlign: TextAlign.center,),
+                //   ),
+                  // Text('Achievements', textScaleFactor: 1.5, textAlign: TextAlign.center,),
+                  
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                // Padding(
+              
+                //   padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                //   child:
+                //   Text('Lists', textScaleFactor: 1.5, textAlign: TextAlign.center,),
+                  
+                // ),
+                ButtonBar(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 150),
+                      child: 
+                        RaisedButton(
+                          onPressed: () {},
+                          color: Colors.lightBlueAccent,
+                          padding: EdgeInsets.all(10),
+                          child:
+                        // Padding(
+                          // padding: EdgeInsets.all(40),
+                          // child:
+                            Text('Chores', textScaleFactor: 1.5, ),
+
+                      // ),
+                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 150),
+                      child: 
+                      RaisedButton(
+                      onPressed: () {},
+                      padding: EdgeInsets.all(10),
+                      color: Colors.lightBlueAccent,
+                      child:
+                      Text('Flat Button', textScaleFactor: 1.5,),
+                    ),
+                    ),
+                    
+                  ],)
+              ],
+            ),
+        ],
+    ),
+      
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
