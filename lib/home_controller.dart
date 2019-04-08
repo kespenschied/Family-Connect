ebfc2cc05020b8b3034710b18eeef7383e942080
kespenschied <kespens@siue.edu>
2019-04-08 07:23:40 -0500

    Moving classes around

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './home_icons_info.dart';
 import './achievements.dart';
 import './books.dart';
 import './chores.dart';
@@ -106,6 +105,53 @@ class HomeController extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HomeIconsInfo extends StatefulWidget {
+  final String imagePath;
+  final String title;
+
+  HomeIconsInfo(this.imagePath, this.title);
+
+  @override
+  _HomeIconsInfoState createState() =>
+      _HomeIconsInfoState(this.imagePath, this.title);
+}
+
+class _HomeIconsInfoState extends State<HomeIconsInfo> {
+  String imagePath;
+  String title;
+
+  _HomeIconsInfoState(this.imagePath, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double deviceWidth = width * 1.00;
+    double deviceHeight = height * 0.2207;
+
+    return Container(
+      width: deviceWidth / 2,
+      height: deviceHeight,
+      alignment: Alignment.bottomRight,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(imagePath),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15.0,
+          color: Colors.grey,
+          background: Paint()..color = Colors.black87,
+        ),
       ),
     );
   }
