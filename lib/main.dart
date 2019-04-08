7cefa0f8642a597e049891a622fe7dc7017ba670
kole <kespens@siue.edu>
2019-04-08 06:31:20 -0500

    Made sure auto rotate was turned off

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './home.dart';
 
@@ -7,6 +8,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Family Connect',
       home: HomePage(),
