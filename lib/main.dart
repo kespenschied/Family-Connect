40db04bf211cb323f8717d81bdab48702c219fc5
SeanMat <smathew808@gmail.com>
2019-04-08 01:19:51 -0500

    Made template of MainMenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import './FirstPage.dart' as first;
+import './MainMenu.dart';
 
 void main() => runApp(new MaterialApp(home: new MyTabs()));
 
 class MyTabs extends StatefulWidget {
   @override
   MyTabsState createState() => new MyTabsState();
-  
 }
 
 class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
- 
- TabController controller;
+  TabController controller;
 
- @override
- void initState() {
+  @override
+  void initState() {
     // TODO: implement initState
     super.initState();
     controller = new TabController(vsync: this, length: 3);
@@ -26,22 +25,29 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
     controller.dispose();
     super.dispose();
   }
- 
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Favourites"),
-        backgroundColor: Colors.green, bottom: new TabBar(
+        backgroundColor: Colors.green,
+        bottom: new TabBar(
           controller: controller,
           tabs: <Tab>[
-          new Tab(icon: Icon(Icons.home)),
-          new Tab(icon: Icon(Icons.looks_one)),
-          new Tab(icon: Icon(Icons.looks_two)),
-          new Tab(icon: Icon(Icons.looks_3))
-        ],),
+            new Tab(icon: Icon(Icons.home)),
+            new Tab(icon: Icon(Icons.looks_one)),
+            new Tab(icon: Icon(Icons.looks_two))
+          ],
+        ),
       ),
+      body: MainMenuButtons(),
     );
   }
 }
+
+
+
+
+
