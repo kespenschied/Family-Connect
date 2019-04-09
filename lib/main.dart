b0360ad9d651f098cdc63ee0c77c0155b0e4997b
SeanMat <smathew808@gmail.com>
2019-04-08 22:09:48 -0500

    Almost done with Chorechart

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ SystemChrome.setPreferredOrientations([
 
     return MaterialApp(
       theme: ThemeData(
-       brightness: Brightness.dark,
-        //primarySwatch: Colors.deepOrange,
-        //accentColor: Colors.yellowAccent
+       brightness: Brightness.light,
+        primarySwatch: Colors.green,
+        accentColor: Colors.yellowAccent
     ),
     home: Scaffold(
       appBar: AppBar(
@@ -39,15 +39,182 @@ SystemChrome.setPreferredOrientations([
       ),
       body: Column(children: <Widget>[ //this column be made up of several rows, where each row will have several containers
         Row( children: <Widget>[
-           Text(
+
+          Card(child:Container(
+
+            margin: const EdgeInsets.all(20.0),
+
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
               'Chores',
               textDirection: TextDirection.ltr,
               style: TextStyle(
                 fontFamily: 'Raleway',
-                fontSize: 20,
-                background: Paint()..color = Colors.white24,
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
               ),
             ),
+           ),
+          ),
+          ),
+          new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Mon',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+        new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Tue',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+        new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Wed',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+        new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Thur',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+
+        new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Fri',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+
+        new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Sat',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+
+        new Flexible(
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+           child:Text(
+              'Sun',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+        ),
+        ],
+
+        ), //Chores, and Days of week end here
+
+        //start a new row
+
+        Row( children: <Widget>[
+          Card(child:Container(
+
+            margin: const EdgeInsets.all(20.0),
+           // width: 40.0,
+
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
+              'Trash',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
+
+          Card(
+            child: Icon(Icons.star, size: 40)
+          ) 
+        ],
+
+        ),
+
+        Row( children: <Widget>[
+         Card(child:Container(
+
+            margin: const EdgeInsets.all(20.0),
+           // width: 40.0,
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
+              'Dishes',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
         ],
 
         )
