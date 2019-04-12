cd8b6d45db20f4bd5609488f46d69184b8491f01
SeanMat <smathew808@gmail.com>
2019-04-11 20:48:08 -0500

    Homework finished

--- lib/homework.dart
+++ lib/homework.dart
@@ -44,11 +44,230 @@ dispose(){
       appBar: AppBar(
         centerTitle: true,
         title: Text('Homework'),
-        backgroundColor: Colors.black,
+        //backgroundColor: Colors.black,
       ),
       endDrawer: MyDrawer(),
-        body: 
+        body: ListView(children: <Widget>[ //this column be made up of several rows, where each row will have several containers
+        Row( children: <Widget>[
+
+          Card(child:Container(
+            margin: const EdgeInsets.all(30.0),
+
+           child: SizedBox(
+          //width: 80.0,
+         // height: 20.0,
+           child:Text(
+              'Days',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                color: Colors.orange,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
+         HomeworkContainerTitle('Mon'),
+         HomeworkContainerTitle('Tue'),
+         HomeworkContainerTitle('Wed'),
+         HomeworkContainerTitle('Thu'),
+         HomeworkContainerTitle('Fri'),
+         HomeworkContainerTitle('Sat'),
+         HomeworkContainerTitle('Sun')
+        
+        ],
+
+        ), //Chores, and Days of week end here
+
+        //start a new row
+
+        Row( children: <Widget>[
+          Card(child:Container(
+
+            margin: const EdgeInsets.all(18.0),
+           // width: 40.0,
+
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
+              'Math',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
+
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+
+        ],
+
+        ),
+
+        Row( children: <Widget>[
+         Card(child:Container(
+
+            margin: const EdgeInsets.all(18.0),
+           // width: 40.0,
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
+              'Read',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
+
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+
+        ],
+
+        ),
+
+        Row( children: <Widget>[
+         Card(child:Container(
+
+            margin: const EdgeInsets.all(18.0),
+           // width: 40.0,
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
+              'Sci',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
+
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+
+        ],
+
+        ),
+
+        Row( children: <Widget>[
+         Card(child:Container(
+
+            margin: const EdgeInsets.all(18.0),
+           // width: 40.0,
+           child: SizedBox(
+          width: 80.0,
+          height: 20.0,
+           child:Text(
+              'History',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 25,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          ),
+          ),
+
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+         StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+          StarButtonIcon(Icons.star, 42, Colors.green),
+
+        ],
+
+        )
+      ],),
 
     );
+  }
+}
+
+
+class HomeworkContainerTitle extends StatelessWidget {
+
+  final String titleText;
+
+  //use 
+  HomeworkContainerTitle(this.titleText);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(
+          width: 50.0,
+          height: 50.0,
+           child:Card(
+             child: Center(
+           child:Text(
+              titleText,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 20,
+                color: Colors.green,
+                //background: Paint()..color = Colors.white12,
+              ),
+            ),
+           ),
+          )
+         );
+  }
+}
+
+class StarButtonIcon extends StatelessWidget{
+
+final IconData image;
+final int size;
+final Color color;
+
+StarButtonIcon(this.image, this.size, this.color);
+
+
+@override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+              child: Icon(
+                Icons.star, size: 42, color: Colors.green,)
+          );
   }
 }
