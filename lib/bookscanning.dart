d8a53c664202c81c2a21b69b16ecbfe95dbb3a09
Kole <kespens@siue.edu>
2019-09-02 10:44:15 -0500

    added all the skeletons for the remaining pages we have to design

---
+++ lib/bookscanning.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BookscanningPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Scan Book"),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
---
+++ lib/editusers.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Edit Users"),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        // Add box decoration
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.5, 0.7, 0.9],
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.grey[400],
+              Colors.grey[600],
+              Colors.grey[500],
+              Colors.grey[400],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
