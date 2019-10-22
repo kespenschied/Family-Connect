85e5c66424c31970ce8d3256e5293f94c93e44e5
Megan Ramaker <mramake@siue.edu>
2019-10-22 10:15:31 -0500

    adding locator class

---
+++ lib/coreClasses/locator.dart
@@ -0,0 +1,19 @@
+//DB
+//By: Sean Mathews
+//October 13th, 2019
+
+//********************************************************************
+//This file is used as the initital connection to the firebase DB, 
+//which passes the Users collection to the file "userCRUD.dart" for 
+//CRUD operations
+//********************************************************************
+import 'package:family_connect/Utilities/UserCRUD.dart';
+import 'package:get_it/get_it.dart';
+import 'api.dart';
+
+GetIt locator = GetIt();
+
+void setupLocator() {
+  locator.registerLazySingleton(() => Api('Users')); //path that leads to the collection, Users, on fireStore. will need a bunch of these for all the collections
+  locator.registerLazySingleton(() => UserCRUD());
+}
